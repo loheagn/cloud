0734d4a2a0102a07eaa4833e2abf267de8efd77f
loheagn <loheagn@icloud.com>
2021-08-14 15:35:26 +0800

    1. 添加 image.Push 方法 2. 添加对 image 相关方法的output信息的错误处理

--- docker/image/image.go
+++ docker/image/image.go
@@ -1,7 +1,12 @@
 package image
 
 import (
+	"bufio"
+	"bytes"
 	"context"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
 	"io"
 
 	"github.com/docker/docker/api/types"
@@ -16,12 +21,12 @@ type BuildOption struct {
 	Tags           []string
 }
 
-func Build(ctx context.Context, opt *BuildOption) (io.ReadCloser, error) {
+func Build(ctx context.Context, opt *BuildOption) (string, error) {
 	cli, err := docker.GetClient(&docker.InitOption{
 		Host: opt.HostURL,
 	})
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	buildOpts := types.ImageBuildOptions{
 		Dockerfile: opt.DockerFilePath,
@@ -31,8 +36,80 @@ func Build(ctx context.Context, opt *BuildOption) (io.ReadCloser, error) {
 
 	resp, err := cli.ImageBuild(ctx, buildCtx, buildOpts)
 	if err != nil {
-		return nil, err
+		return "", err
+	}
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(resp.Body)
+
+	return handleOutput(resp.Body)
+}
+
+type PushOption struct {
+	HostURL  string
+	Tag      string
+	Username string
+	Password string
+}
+
+func Push(ctx context.Context, opt *PushOption) (string, error) {
+	cli, err := docker.GetClient(&docker.InitOption{
+		Host: opt.HostURL,
+	})
+	if err != nil {
+		return "", err
 	}
 
-	return resp.Body, err
+	authConfig := types.AuthConfig{
+		Username: opt.Username,
+		Password: opt.Password,
+	}
+	encodedJSON, _ := json.Marshal(authConfig)
+	authStr := base64.URLEncoding.EncodeToString(encodedJSON)
+	resp, err := cli.ImagePush(ctx, opt.Tag, types.ImagePushOptions{
+		All:           false,
+		RegistryAuth:  authStr,
+		PrivilegeFunc: nil,
+	})
+	if err != nil {
+		return "", err
+	}
+	defer func(resp io.ReadCloser) {
+		_ = resp.Close()
+	}(resp)
+	return handleOutput(resp)
+}
+
+type ErrorLine struct {
+	Error       string      `json:"error"`
+	ErrorDetail ErrorDetail `json:"errorDetail"`
+}
+
+type ErrorDetail struct {
+	Message string `json:"message"`
+}
+
+func handleOutput(reader io.Reader) (string, error) {
+	var (
+		lastLine string
+		buff     bytes.Buffer
+	)
+
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		lastLine = scanner.Text()
+		buff.WriteString(lastLine)
+		_ = buff.WriteByte('\n')
+	}
+
+	errLine := &ErrorLine{}
+	_ = json.Unmarshal([]byte(lastLine), errLine)
+	if errLine.Error != "" {
+		return buff.String(), errors.New(errLine.Error)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return buff.String(), err
+	}
+	return buff.String(), nil
 }
